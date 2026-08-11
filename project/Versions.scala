c3c5002fc1d75a41f74cb48abc2281dd2eb37b3c
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-08-11 22:26:51 +0200

    :arrow_up: Otel

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val zio = "2.1.26"
-  val zioOpenTelemetry = "3.1.18"
-  val openTelemetry = "1.64.0"
+  val zioOpenTelemetry = "3.1.19"
+  val openTelemetry = "1.65.0"
   val openTelemetrySemconvVersion = "1.43.0"
 }
